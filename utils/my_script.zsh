7f6bb08977838364e89cfded7b040faa8578acd3
javidominguezsegura <javier.dominguez.segura@gmail.com>
2024-10-11 00:05:33 +0200

    added script

--- utils/my_script.zsh
+++ utils/my_script.zsh
@@ -2,5 +2,6 @@ rm -rf utils/requirements.txt
 rm -rf utils/tree.txt
 
 pip3 freeze > requirements.txt
-cd dsa/
+cd ~
+cd /Users/javierdominguezsegura/Programming/College/Sophomore/Algos/Final_project/dsa
 tree > utils/tree.txt
