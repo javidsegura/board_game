0b6a47ac6c279b5b936ee0b333a4c753a99191ad
javidominguezsegura <javier.dominguez.segura@gmail.com>
2024-10-13 22:39:07 +0200

    refactoring mostly

--- src/grid.py
+++ src/grid.py
@@ -1,27 +1,26 @@
 from PySide6.QtWidgets import QPushButton, QGridLayout, QVBoxLayout, QSpacerItem, QSizePolicy
-
 class GridLogic:
-    def __init__(self, grid_size : int, on_button_click : callable) -> None:
+    def __init__(self, grid_size : int, on_cell_click : callable) -> None:
         """
         Parameters:
             - grid_size: int
             - on_button_click: function
         """
         self.grid_size = grid_size
-        self.buttons = {}
-        self.on_button_click = on_button_click
+        self.cells = {} # Set of all cells in the grid
+        self.on_cell_click = on_cell_click # function to call when ...
 
-    def setup_grid(self):
+    def setup_grid(self) -> QVBoxLayout:
         self.grid_layout = QGridLayout()
         self.grid_layout.setSpacing(10)  # Spacing between cells
 
         for row in range(self.grid_size):
             for col in range(self.grid_size):
-                btn = QPushButton("")
-                btn.setFixedSize(120, 120)
-                btn.clicked.connect(lambda _, r=row, c=col: self.on_button_click(r, c))
-                self.grid_layout.addWidget(btn, row, col)
-                self.buttons[(row, col)] = btn
+                cell = QPushButton("")
+                cell.setFixedSize(120, 120)
+                cell.clicked.connect(lambda _, r=row, c=col: self.on_cell_click(r, c))
+                self.grid_layout.addWidget(cell, row, col)
+                self.cells[(row, col)] = cell
 
         grid_container = QVBoxLayout()
         grid_container.addSpacerItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
@@ -29,20 +28,22 @@ class GridLogic:
         grid_container.addSpacerItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
         return grid_container
+        
+    def disable_grid(self, disable: bool) -> None:
+        for cell in self.cells.values():
+            cell.setDisabled(disable)
 
-    def disable_grid(self, disable=True):
-        for btn in self.buttons.values():
-            btn.setDisabled(disable)
+    def reset_buttons(self) -> None:
+        """ Reset the grid to its initial state for a new game"""
+        for cell in self.cells.values():
+            cell.setText("") # Eliminate icons
+            cell.setEnabled(True) # Enable to click on the cell
+            cell.setStyleSheet("") # Reset style to default version
 
-    def reset_buttons(self):
-        for btn in self.buttons.values():
-            btn.setText("")
-            btn.setEnabled(True)
-            btn.setStyleSheet("")
+    def set_button_state(self, row:int, col:int, text:str, style:str) -> None:
+        """ Changes the text and style of a cell """
+        self.cells[(row, col)].setText(text)
+        self.cells[(row, col)].setStyleSheet(style)
 
-    def set_button_state(self, row, col, text, style):
-        self.buttons[(row, col)].setText(text)
-        self.buttons[(row, col)].setStyleSheet(style)
-
-    def disable_button(self, row, col):
-        self.buttons[(row, col)].setDisabled(True)
+    def disable_button(self, row:int, col:int) -> None:
+        self.cells[(row, col)].setDisabled(True)
